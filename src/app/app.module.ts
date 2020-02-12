a1defe5809ea8666e5a10762e947b3aa2e92a8ef
Steven Dunn <Dunn.Steven1@gmail.com>
2020-02-12 15:41:31 -0800

    Changed TSConfig target to 'ES5' to reflect our real app environment. Added "node" types to TSConfig. Added a provider for the angular2-highcharts HighchartsStatic service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartModule } from 'angular2-highcharts';
+
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
+// See the providers array below.
+export function highchartsFactory() {
+  const hc = require('highcharts');
+  const exporting = require('highcharts/modules/exporting');
+  const offline = require('highcharts/modules/offline-exporting');
+  exporting(hc);
+  offline(hc);
+  return hc;
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +22,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartModule, // Angular2-highcharts Module
   ],
-  providers: [],
+  providers: [
+    // Provide the HighCarts service in the CoreModule so that the service isn't duplicated in lazy-loaded modules
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
