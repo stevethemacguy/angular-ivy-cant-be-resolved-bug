c3f51c144b567477dd588007cf21b8617aa83259
Steven Dunn <Dunn.Steven1@gmail.com>
2020-02-12 16:00:28 -0800

    Created a basic chart.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ivy-cant-be-resolved';
+  options = {
+    chart: {
+      type: 'bar'
+    },
+    series: [{
+      data: [1, 0, 4]
+    }, {
+      data: [5, 7, 3]
+    }]
+  };
 }
